c7346b248f6dd3512c233a25a2321f7814404261
penukonda bharath kumar <pbharath.k@izmoltd.com>
2025-11-13 15:38:21 +0530

    added build warning changes

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -5,17 +5,26 @@ const ItemsList = ({ items }) => {
     <div>
       {items &&
         items.map((item) => (
-          <div key={item.id} className="p-4 border-b">
-            <h2 className="font-semibold text-lg">{item?.card?.info?.name}</h2>
-            <img
-              src={`${CDN_URL}${item?.card?.info?.imageId}`}
-              alt={item?.card?.info?.name}
-              className="w-1/12 "
-            />
-            <p className="text-gray-600">{item?.card?.info?.description}</p>
-            <span className="text-green-600 font-bold">
-              {item?.card?.info?.price / 100}
-            </span>
+          <div
+            key={item.id}
+            className="p-4 border-b flex-row flex justify-between"
+          >
+            <div className="">
+              <h2 className="font-semibold text-lg">
+                {item?.card?.info?.name}
+              </h2>
+
+              <p className="text-gray-600">{item?.card?.info?.description}</p>
+              <span className="text-green-600 font-bold">
+                {item?.card?.info?.price / 100}
+              </span>
+            </div>
+            <div className="w-1/6 justify-end">
+              <img
+                src={`${CDN_URL}${item?.card?.info?.imageId}`}
+                alt={item?.card?.info?.name}
+              />
+            </div>
           </div>
         ))}
     </div>
--- src/components/RestMenu.js
+++ src/components/RestMenu.js
@@ -2,22 +2,23 @@ import { useEffect, useState } from "react";
 import { API_URL } from "../constants";
 import { useParams } from "react-router-dom";
 import ItemsList from "./ItemsList";
+import { useCallback } from "react";
 
 const RestMenu = () => {
   const [restMenu, setRestMenu] = useState(null);
   const { resId } = useParams();
-  useEffect(() => {
-    //API Call
-    fetchResItemData();
-  }, []);
-  const fetchResItemData = async () => {
+  const fetchResItemData = useCallback(async () => {
     let data = await fetch(`${API_URL}/listRestaurantMenu/${resId}`);
     const jsonData = await data.json();
     console.log("menu", jsonData);
     setRestMenu(
       jsonData?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards
     );
-  };
+  }, [resId]);
+  useEffect(() => {
+    //API Call
+    fetchResItemData();
+  }, [fetchResItemData]);
   return (
     <div className="border w-6/12 justify-center items-center mx-auto">
       {restMenu &&
